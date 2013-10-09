c28cc15ade21b6f3ac985ece8ba847d7bbecb386
macguy2010 <nateaston2010@gmail.com>
2013-10-08 20:58:04 -0400

    Beginning of the Project

---
+++ QuizBuilderProject/src/main/java/com/nate_tim_dane_company/quizbuilderproject/Question.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nate_tim_dane_company.quizbuilderproject;
+
+import java.io.Serializable;
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+public class Question {
+    
+    // variables
+    @Id @GeneratedValue
+    private Long id;
+    
+    private String question;
+    
+    private String answer;
+    
+    @Enumerated(EnumType.ORDINAL)
+    private SubjectType subject;
+    
+    private int difficulty;
+    
+    
+    // methods
+    public Long getId()
+    {
+        return id;
+    }
+    
+    public String getQuestion()
+    {
+        return question;
+    }
+    
+    public void setQuestion(String q)
+    {
+       question = q;
+    }
+    
+    public String getAnswer()
+    {
+        return answer;
+    }
+    
+    public void setAnswer(String a)
+    {
+       answer = a;
+    }
+    
+    public int getDifficulty()
+    {
+        return difficulty;
+    }
+    
+    public void setDifficulty(int d)
+    {
+        difficulty = d;
+    }
+    
+    public SubjectType getSubject()
+    {
+        return subject;
+    }
+    
+    public void setSubject(SubjectType st)
+    {
+        subject = st;
+    }
+}
---
+++ QuizBuilderProject/src/main/java/com/nate_tim_dane_company/quizbuilderproject/SubjectType.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nate_tim_dane_company.quizbuilderproject;
+
+import javax.persistence.*;
+
+public enum SubjectType {
+    MATHEMATICS,
+    ENGLISH,
+    SCIENCE,
+    GEOGRAPHY,
+    HISTORY,
+    LITERATURE,
+    COMPUTER_SCIENCE
+}
