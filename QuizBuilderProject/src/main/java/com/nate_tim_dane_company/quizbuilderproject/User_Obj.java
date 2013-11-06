0be98637decd11423966c6e0ced972eb4ba8af36
macguy2010 <nateaston2010@gmail.com>
2013-11-06 15:14:55 -0500

    Populating Database

--- QuizBuilderProject/src/main/java/com/nate_tim_dane_company/quizbuilderproject/User_Obj.java
+++ QuizBuilderProject/src/main/java/com/nate_tim_dane_company/quizbuilderproject/User_Obj.java
@@ -13,8 +13,9 @@ import javax.persistence.*;
  * @author nathanaston
  */
 @Entity
+@Table(name = "User_Obj")
 @NamedQuery(name = "findAllUsers", query = "SELECT u FROM User_Obj u")
-public class User_Obj implements Serializable {
+public class User implements Serializable {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
