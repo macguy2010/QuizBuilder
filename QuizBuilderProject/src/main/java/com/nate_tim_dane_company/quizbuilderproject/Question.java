791542d93af3dea196838807a8083829e404776f
macguy2010 <nateaston2010@gmail.com>
2013-12-05 22:13:06 -0500

    FIXEDDDD

--- QuizBuilderProject/src/main/java/com/nate_tim_dane_company/quizbuilderproject/Question.java
+++ QuizBuilderProject/src/main/java/com/nate_tim_dane_company/quizbuilderproject/Question.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Entity
 @NamedQueries({
 @NamedQuery(name = "findAllQuestions", query = "SELECT q FROM Question q"),
-@NamedQuery(name = "findAllQuestionsById", query = "SELECT q FROM Question q where q.userid = :ID"),
+@NamedQuery(name = "findAllQuestionsById", query = "SELECT q FROM Question q where q.userId = :ID"),
 })
 
 public class Question implements Serializable{
